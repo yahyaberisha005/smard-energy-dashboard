a1a942291d63dacf74c9318684ef8393ffde1289
yahyaberisha005 <yahyaberisha05@gmail.com>
2026-02-06 21:12:58 +0100

    Update queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,1 +1,14 @@
+-- =========================================================
+-- SMARD Energy Dashboard - SQL Query Pack
+-- Author: <dein Name>
+-- =========================================================
 
+-- 01) Basic selects & cleaning
+
+-- 02) Aggregations (daily/weekly/monthly)
+
+-- 03) Joins (fact + dimensions)
+
+-- 04) Window functions (rolling avg, ranking)
+
+-- 05) KPI queries (MoM, YoY, volatility, peak detection)
